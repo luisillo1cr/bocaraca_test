4316dc621f8ad77baadb6b1af60d9951292ae730
Luis David S <54914200+luisillo1cr@users.noreply.github.com>
2025-04-30 13:53:29 -0600

    update_registro_0.0.18

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,6 @@
 // Importar las bibliotecas de Firebase necesarias
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";  // Importar Realtime Database
@@ -8,16 +9,17 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-
 const firebaseConfig = {
     apiKey: "AIzaSyD6CUddDq2jRZ3XzNT5FfXAc-ALStQ4hdo",
     authDomain: "bocaracadb.firebaseapp.com",
-    databaseURL: "https://bocaracadb-default-rtdb.firebaseio.com/",  // Asegúrate de usar la URL de tu Realtime Database
+    databaseURL: "https://bocaracadb-default-rtdb.firebaseio.com",
     projectId: "bocaracadb",
-    storageBucket: "bocaracadb.appspot.com",
+    storageBucket: "bocaracadb.firebasestorage.app",
     messagingSenderId: "420681635696",
     appId: "1:420681635696:web:c4c1a47be59fb45912a1b5",
     measurementId: "G-QZS4RL1H5Q"
-};
+  };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Inicializar Auth, Firestore y Realtime Database
 const auth = getAuth(app);
